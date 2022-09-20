243a9b9d88cbcf3371f4614ba327ec73fcc549d6
pong dev beginer <patiphonpracticwork001@gmail.com>
2022-09-20 23:28:54 +0700

    add change session

--- set_cart.php
+++ set_cart.php
@@ -3,6 +3,9 @@ session_start();
 $x = isset($_POST['id']) ? $_POST['id'] : "";
 
 if (isset($_SESSION['data']) != "") {
+  usort($_SESSION['data'], function ($a, $b) {
+    return $a['id'] - $b['id'];
+  });
   array_splice($_SESSION['data'], $x, 1);
   $array = $_SESSION['data'];
   $count  = count($array) > 0  ?  count($array) : 0;
